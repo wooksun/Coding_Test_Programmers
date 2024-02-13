f8cf169c366bcd1fba5278c073b2eba4ea39b6a3
wooksun <uk1231@naver.com>
2024-02-13 21:50:04 +0900

    Programmers_문자열 출력

--- Coding_Test_Programmers/src/programmers/test/lv0/Lv0_1.java
+++ Coding_Test_Programmers/src/programmers/test/lv0/Lv0_1.java
@@ -1,8 +1,8 @@
-package programmers.test;
+package programmers.test.lv0;
 
 import java.util.Scanner;
 
-public class HelloWolrd {
+public class Lv0_1 {
 
 	public static void main(String[] args) {
 		 Scanner sc = new Scanner(System.in);
