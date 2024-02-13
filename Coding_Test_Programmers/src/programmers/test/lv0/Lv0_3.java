1db980e3d874907cc21c15411517a1133f5462de
wooksun <uk1231@naver.com>
2024-02-13 22:12:29 +0900

    Programmers_문자열 반복해서 출력하기

---
+++ Coding_Test_Programmers/src/programmers/test/lv0/Lv0_3.java
@@ -0,0 +1,16 @@
+package programmers.test.lv0;
+
+import java.util.Scanner;
+
+public class Lv0_3 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		int n = sc.nextInt();
+		
+		for(int i=1; i<=n; i++) {
+			System.out.println(""+str);
+		}
+	}
+}
