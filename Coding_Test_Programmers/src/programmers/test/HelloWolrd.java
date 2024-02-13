4046f989ad451c8bf0ade7f7358f5c6c21696422
wooksun <uk1231@naver.com>
2024-02-13 21:37:50 +0900

    Programmers - 문자열 출력

---
+++ Coding_Test_Programmers/src/programmers/test/HelloWolrd.java
@@ -0,0 +1,14 @@
+package programmers.test;
+
+import java.util.Scanner;
+
+public class HelloWolrd {
+
+	public static void main(String[] args) {
+		 Scanner sc = new Scanner(System.in);
+	     String a = sc.next();
+	        
+	     System.out.println("" + a);
+	}
+
+}
