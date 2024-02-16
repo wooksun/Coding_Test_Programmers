12eff2811fdb04df6fdd06800cb96a4fd593c439
wooksun <uk1231@naver.com>
2024-02-16 21:05:13 +0900

    Programmers_덧셈식 출력하기

---
+++ Coding_Test_Programmers/src/programmers/test/lv0/LV0_6.java
@@ -0,0 +1,20 @@
+package programmers.test.lv0;
+
+import java.util.Scanner;
+
+//	두 정수 a, b가 주어질 때 다음과 같은 형태의 계산식을 출력하는 코드를 작성해 보세요.
+//	a + b = c
+//	제한사항: 1 ≤ a, b ≤ 100
+//	입출력 예) 입력: 4 5, 출력: 4 + 5 = 9
+
+public class LV0_6 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.println(a + "+" + b + "=" + (a+b));
+	}
+
+}
